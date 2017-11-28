1b80f4ee30ac4cce489b92c116c1eccdcff7e122
Ariel Burriss <ariburr@gmail.com>
2017-11-28 12:18:25 -0700

    clean up model methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,17 @@
 class Item < ApplicationRecord
 
   def self.check_complete
-    Item.where("complete = 'true'")
+    Item.where(complete: true)
   end
 
   def self.check_incomplete
-    Item.where("complete = 'false'")
+    Item.where(complete: false)
   end
 
   def self.check_progress
-    comp_ct = Item.where("complete = 'true'").count.to_f
+    comp_ct = Item.where(complete: true).count.to_f
     total = Item.all.count
-    (comp_ct / total) * 100
+    ((comp_ct / total) * 100).round
   end
 
 end
